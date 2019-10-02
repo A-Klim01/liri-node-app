664782e05e4753b75eb3f18f7f04f4c3bca2f227
Alojzy Klim <adklim@gmail.com>
2019-10-01 19:11:06 -0500

    refining code with TA

--- liri.js
+++ liri.js
@@ -4,30 +4,56 @@ require("dotenv").config();
 // Link keys.js page
 var keys = require("./keys.js");
 
-let request = require("request");
+// let request = require("request");
 
 // require file systems
-var fs = require("fs");
+// var fs = require("fs");
 
 //initialize Spotify
-var spotify = new Spotify(keys.spotify);
-var spotify = require("node-spotify-npi");
+// var spotify = new Spotify(keys.spotify);
+// var spotify = require("node-spotify-npi");
 
 // Connects omdb & bandsintown API's
 let omdb = (keys.omdb);
-let bandsintown = (keys.bandsintown);
+//we are using the public key so dont need to hide it
+// let bandsintown = (keys.bandsintown);
 
 //processe's user inputs
 let userInput = process.argv[2];
-let userQuery = process.argv. slice[3].join(" ");
+let userQuery = process.argv.slice(3).join(" ");
 
 
-function userCommand(userInput, userQuery) 
+function userCommand(userInput){
+
+    // console.log("user command is: " + userInput)
+    // console.log("user query is: " + userQuery)
+
+    if (userInput === "spotify-this"){
+        spotifyThisSong()
+    }
+} 
 
 userCommand(userInput, userQuery);
 
-function spotifyThisSong();
+function spotifyThisSong(){
+    //code goes here
+    console.log("spotify this song fired off")
+    //can build axios calls here
+    console.log(userQuery)
 
-function movieThis();
+};
 
-function whatThis();
+function movieThis(){
+    //code goes here
+
+};
+
+function whatThis(){
+    //code goes here
+
+}
+;
+//"https://rest.bandsintown.com/artists/" + userQuery + "?app_id=codingbootcamp";
+
+//use this for testing 
+//"https://rest.bandsintown.com/artists/pink?app_id=codingbootcamp";
