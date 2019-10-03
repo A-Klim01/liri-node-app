1a7bc89cb8077062368e8b148a8ed170383bbb51
Alojzy Klim <adklim@gmail.com>
2019-10-02 21:59:14 -0500

    refined api call code

--- liri.js
+++ liri.js
@@ -4,6 +4,7 @@ require("dotenv").config();
 // Link keys.js page
 var keys = require("./keys.js");
  let request = require("request");
+ var axios = require("axios");
 
 // require file systems
  var fs = require("fs");
@@ -18,32 +19,35 @@ var Spotify = require("node-spotify-api");
  
  
 
-// Connects omdb & bandsintown API's
+// Connects omdb 
 let omdb = (keys.omdb);
-//we are using the public key so dont need to hide it
-// let bandsintown = (keys.bandsintown);
 
 //processe's user inputs
 let userInput = process.argv[2];
 let userQuery = process.argv.slice(3).join(" ");
 
 
-function userCommand(userInput){
+function userCommand(userInput, userQuery){
 
-    // console.log("user command is: " + userInput)
-    // console.log("user query is: " + userQuery)
+   
+    switch(userInput){
+        case "spotify-this": spotifyThisSong(userQuery);
+        break;
+        case "concert-this": concertThis(userQuery);
+        break;
+        case "movie-this": moviethis(userQuery);
+        break;
+       
+        case "do-what-it-says": dowhatItSays();
+        break;
+        default: console.log("I don't know what you're asking")
 
-    if (userInput === "spotify-this"){
-        spotifyThisSong();
-        //break;
-        //case "movie-this"():
-            //movieThis();
     }
 } 
 
 userCommand(userInput, userQuery);
 
-function spotifyThisSong(){
+function spotifyThisSong(userQuery){
     console.log("spotify this song fired off")
     spotify.search({ type:'track' , query: userQuery }, function (error, data) {
         if (error) {
@@ -57,14 +61,42 @@ function spotifyThisSong(){
     }
 
 ;
+function concertThis(userQuery){
+    //https://rest.bandsintown.com/artists/" + artist + "/events?app_id=codingbootcamp
+    var queryURL = "https://rest.bandsintown.com/artists/" + userQuery + "/events?app_id=codingbootcamp";
+    axios.get(queryURL).then(function(response){
+        //console.log(response);
+        var showInfo = response.data;
+        for(var i=0; i<showInfo.length; i++){
+            var show = showInfo[i];
 
-function movieThis(){
-    //code goes here
+            console.log("venue country:  " +show.venue.country + " and  Venue city : " +show.venue.city);
+            console.log("show date and time: " +show.datetime);
+            console.log("venue name: " +show.venue.name);
+            console.log("-----------------------------------------")
+        }
+    })
+
+}
+
+function moviethis(userQuery){
+    let omdb = (keys.omdb);
+    axios.get(queryURL).then(function(response){
+        console.log(response.data);
+    })
 
 };
 
-function whatThis(){
+function dowhatItSays(){
     //code goes here
+    fs.readFile("random.txt", "utf8", function(err, data){
+        if(err){
+            console.log(err)
+        }
+        var dataArr = data.split(",");
+        userCommand(dataArr[0], dataArr[1])
+
+    })
 
 }
 ;
